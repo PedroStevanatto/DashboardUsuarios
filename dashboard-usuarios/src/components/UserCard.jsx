81c6ee7873d96a9a5c7b89948b783d039a019aaf
PedroStevanatto <phstev06@gmail.com>
2025-06-06 11:27:24 -0300

    Revert to 56e05fced

--- dashboard-usuarios/src/components/UserCard.jsx
+++ dashboard-usuarios/src/components/UserCard.jsx
@@ -1,23 +1,15 @@
 // src/components/UserCard.jsx
 import './UserCard.css';
 
-//o <a> </a> vai ser o que torna possível clicar na imagem 
 function UserCard({ user }) {
     return (
-        <a onClick={clicar}>
-            <div className='user-card'>
-                <img src={user.avatar} alt={`${user.firstName} avatar`} />
-                <h3>{user.firstname} {user.lastname}</h3>
-                <p>{user.email}</p>
-                <small>{user.address}</small>
-            </div>
-        </a>
+        <div className='user-card'>
+            <img src={user.avatar} alt={`${user.firstName} avatar`} />
+            <h3>{user.firstName} {user.lasName}</h3>
+            <p>{user.email}</p>
+            <small>{user.address}</small>
+        </div>
     )
-}
-//manipulador de eventos que faz algo ao clicar no <a><a/>
-const clicar = (event) => {
-    event.preventDefault(); //evita que a pagina do dashboard recarregue
-    window.open(event.target.href, '_blank', 'noopener,noreferrer');
 }
 
 export default UserCard;
