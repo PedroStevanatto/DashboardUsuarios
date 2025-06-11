54d963545a1e635070f8af32c3befbb6dca00e68
PEDRO HENRIQUE FONSECA CUSTODIO STEVANATTO <pedro.stevanatto@etec.sp.gov.br>
2025-06-11 08:37:55 -0300

    Correção mínima

--- dashboard-usuarios/src/components/UserCard.jsx
+++ dashboard-usuarios/src/components/UserCard.jsx
@@ -4,8 +4,8 @@ import './UserCard.css';
 function UserCard({ user }) {
     return (
         <div className='user-card'>
-            <img src={user.avatar} alt={`${user.firstName} avatar`} />
-            <h3>{user.firstName} {user.lasName}</h3>
+            <img src={user.avatar} alt={`${user.firstname} avatar`} />
+            <h3>{user.firstname} {user.lastname}</h3>
             <p>{user.email}</p>
             <small>{user.address}</small>
         </div>
